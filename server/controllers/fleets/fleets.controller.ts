538d8281f47fb99d8ebe89b9fcc8e4aef9f09e65
Inbar Shani <inbar@outgage.co>
2022-01-22 15:39:14 +0200

    Server query vessels

--- server/controllers/fleets/fleets.controller.ts
+++ server/controllers/fleets/fleets.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { plainToClass } from 'class-transformer';
 import { ParamId } from '../../decorators/params.decorator';
+import { FleetQuery } from '../../models/fleet';
 import { FleetsService } from '../../services/fleets/fleets.service';
 
 @Controller('fleets')
@@ -7,8 +9,13 @@ export class FleetsController {
     constructor(private fleetsService: FleetsService) {}
 
     @Get()
-    async getFleets() {
-        return await this.fleetsService.getFleets();
+    async getFleets(@Query('filter') filter: string) {
+        if (filter) {
+            const fleetQuery = plainToClass(FleetQuery, JSON.parse(filter), { excludeExtraneousValues: true });
+            return await this.fleetsService.getFleets(fleetQuery);
+        } else {
+            return await this.fleetsService.getFleets();
+        }
     }
 
     @Get(':id')
