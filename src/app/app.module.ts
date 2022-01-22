6c0496f649822b2f2a0c24cf854a9273bee8fa11
Inbar Shani <inbar@outgage.co>
2022-01-22 16:21:55 +0200

    Tables filtering

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +15,15 @@ import { FleetComponent } from './features/fleets/fleet/fleet.component';
 
 @NgModule({
     declarations: [AppComponent, PageNotFoundComponent, FleetsTableComponent, FleetComponent],
-    imports: [BrowserModule, AppRoutingModule, HttpClientModule, TableModule, InputTextModule, ButtonModule],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        TableModule,
+        InputTextModule,
+        ButtonModule,
+        BrowserAnimationsModule
+    ],
     providers: [],
     bootstrap: [AppComponent]
 })
